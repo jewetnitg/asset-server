1540753e30f0e892bd0118f7e0402d42888fa0b5
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2015-12-05 23:23:10 +0100

    removed src module, build now clones the repo every time

--- tasks/assets.js
+++ tasks/assets.js
@@ -4,17 +4,13 @@ var serverConfig = require('../server.config');
 
 // this task builds the static assets, it really builds a (git) submodule
 module.exports = function (gulp) {
-  var nodeModulesPath = path.join(serverConfig.remote.dir, 'node_modules');
-  var gulpfilePath = path.join(serverConfig.remote.dir, serverConfig.remote.gulpFile);
-  var srdDstPath = path.join(serverConfig.remote.dir, serverConfig.remote.dstDir);
   var tempDir = '.asset-server-tmp';
+  var srcDstPath = path.join(tempDir, serverConfig.remote.dstDir);
+  var nodeModulesPath = path.join(tempDir, 'node_modules');
+  var gulpfilePath = path.join(tempDir, serverConfig.remote.gulpFile);
+
   var rmrf = 'rm -rf ';
-  var baseGitCommand = 'git -C ' + serverConfig.remote.dir + ' ';
-  // workaround for cp path/* and mv path/* not working for some reason
-  var dstSuffix = srdDstPath.split(/\/|\\/g);
-  var tmpDstPath = dstSuffix
-    ? path.join(tempDir, dstSuffix[dstSuffix.length - 1])
-    : tempDir;
+  var gitInTempDir = 'git -C ' + tempDir + ' ';
 
   gulp.task('assets', function (done) {
     shell.series([
@@ -24,24 +20,20 @@ module.exports = function (gulp) {
       rmrf + tempDir,
       'mkdir ' + tempDir,
 
-      // set the origin
-      baseGitCommand + 'remote set-url ' + serverConfig.remote.name + ' ' + serverConfig.remote.url,
-
       // pull the submodule
-      baseGitCommand + 'checkout ' + serverConfig.remote.branch,
-      baseGitCommand + 'pull ' + serverConfig.remote.name + ' ' + serverConfig.remote.branch,
-      baseGitCommand + 'submodule update --init --recursive',
+      'git clone ' + serverConfig.remote.url + ' ' + tempDir,
+      gitInTempDir + 'checkout ' + serverConfig.remote.branch,
+      gitInTempDir + 'pull ' + serverConfig.remote.name + ' ' + serverConfig.remote.branch,
+      gitInTempDir + 'submodule update --init --recursive',
 
       // npm install the submodule
-      'npm i --prefix ' + serverConfig.remote.dir,
+      'npm i --prefix ' + tempDir,
 
       // run the submodules build
       'gulp --gulpfile ' + gulpfilePath + ' ' + serverConfig.remote.gulpTask,
 
-      // copy the submodules build to the temp dir, this is a workaround for /* not working for some reason
-      'cp -R ' + srdDstPath + ' ' + tempDir,
       // move the build to its destination
-      'mv ' + tmpDstPath + ' ' + serverConfig.dst,
+      'mv ' + srcDstPath + ' ' + serverConfig.dst,
 
       // clean up filesystem
       rmrf + tempDir
